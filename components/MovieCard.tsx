66255a28815385e1e33e6cf9edd0cde09842dad3
Jerome <jdeboysere@cblue.be>
2025-12-01 13:44:41 +0100

    feat: add favorites system with Zustand and setup shadcn/ui

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import { MovieType } from "@/lib/schemas/movie";
+import { Button } from "@/components/ui/button";
+import { useFavoriteStore } from "@/lib/store/useFavoriteStore";
 
 const TMDB_IMAGE_BASE = process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_URL;
 
@@ -8,6 +12,8 @@ interface MovieCardProps {
 }
 
 export function MovieCard({ movie }: MovieCardProps) {
+  const { toggleFavorite, isFavorite } = useFavoriteStore();
+
   return (
     <div className="overflow-hidden rounded-lg bg-white shadow-md transition-shadow hover:shadow-xl">
       <div className="relative aspect-[2/3] w-full bg-gray-200">
@@ -26,8 +32,11 @@ export function MovieCard({ movie }: MovieCardProps) {
       </div>
 
       <div className="p-4">
-        <h2 className="mb-2 line-clamp-2 text-lg font-semibold text-gray-800">
+        <h2 className="mb-2 line-clamp-2 flex justify-between gap-2 text-lg font-semibold text-gray-800">
           {movie.title}
+          <Button onClick={() => toggleFavorite(movie.id)} variant="outline">
+            {isFavorite(movie.id) ? "❤️" : "🤍"}
+          </Button>
         </h2>
 
         <p className="mb-3 line-clamp-3 text-sm text-gray-600">
